2b9c3f7ceb0f66480397c88f42b1b60014fb7c49
DESKTOP-RFR5C2R\Lab-PC <pei_yachao@yahoo.com>
2016-09-14 22:18:14 +0900

    add  pcl passThrough  fuction

--- realsense_filter/src/realsense_cloud_voxel_filtering.cpp
+++ realsense_filter/src/realsense_cloud_voxel_filtering.cpp
@@ -8,6 +8,7 @@ clobbers voxels with fewer than a threshold of points.
 #include <pcl/point_types.h>
 #include <iostream>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 typedef pcl::PointXYZ PointXYZ;
@@ -26,7 +27,8 @@ class FilterAndPublish
         void callback(const PointCloud::ConstPtr& msg)
         {
             PointCloud::Ptr cloud (new PointCloud);
-            PointCloud::Ptr cloud_filtered (new PointCloud);
+            PointCloud::Ptr cloud_passThrough (new PointCloud);
+            PointCloud::Ptr vox_cloud (new PointCloud);
             *cloud = *msg;
 
             // What to do here: 
@@ -34,17 +36,25 @@ class FilterAndPublish
             // 2. Go through the voxels and remove all points in a voxel that has less than this.thresh points
             // 3. Publish resulting cloud
 
-            pcl::VoxelGrid<PointXYZ> vox;
-            vox.setInputCloud(cloud);
-            // The leaf size is the size of voxels pretty much. Note that this value affects what a good threshold value would be.
-            vox.setLeafSize(0.01f, 0.1f, 0.01f);
-            // I limit the overall volume being considered so lots of "far away" data that is just terrible doesn't even have to be considered.
-            vox.setFilterLimits(-1.0, 1.0);
-            // The line below is perhaps the most important as it reduces ghost points.
-           // vox.setMinimumPointsNumberPerVoxel(this->thresh);
-            vox.filter(*cloud_filtered);
             
-            pub.publish (cloud_filtered);
+            pcl::PassThrough<PointXYZ> pth;
+            pth.setInputCloud(cloud);
+            // The leaf size is the size of voxels pretty much. Note that this value affects what a good threshold value would be.
+            
+            // I limit the overall volume being considered so lots of "far away" data that is just terrible doesn't even have to be considered.
+            pth.setFilterLimits(-1.0, 1.0);
+            // The line below is perhaps the most important as it reduces ghost points.
+           // pth.setMinimumPointsNumberPerVoxel(this->thresh);
+            pth.filter(*cloud_passThrough);
+            
+            pcl::VoxelGrid<PointXYZ> vox;
+            
+            vox.setInputCloud(cloud_passThrough);
+            vox.setLeafSize(0.01f, 0.1f, 0.01f);
+            vox.setFilterLimits(-1.0, 1.0);
+            vox.filter(*vox_cloud);
+            
+            pub.publish (vox_cloud);
         }
 
     private:
